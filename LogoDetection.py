3e9801e5dd2503ac9e03d189ca396de5502c05a7
R. ALOUI <aloui.radhouane@gmail.com>
2023-02-22 14:38:00 +0100

    Add comments

--- LogoDetection.py
+++ LogoDetection.py
@@ -1,35 +1,40 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("images/img1.jpg")
+# Read logo image
+logoFileName = "images/Logo.jpg"
+img = cv2.imread(logoFileName)
+# Set contrast & brightness
 alpha = 0.4
 beta = -10
 
-
-
-
+# Reduce contrast & brightness
 result = cv2.addWeighted(img,alpha,np.zeros(img.shape,img.dtype),0,beta)
+# Convert to grayscale image
 gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+# Convert to binary image
 ret,thresh = cv2.threshold(gray,70,255,0)
+# Display the logo
 cv2.imshow("AI",thresh)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Find objects (All objects in the logo)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-rectanglesList = []
 
+rectanglesList = []
 # Look for the outer bounding boxes (no children):
 for _, c in enumerate(contours):
 
     # Get blob area:
     currentArea = cv2.contourArea(c)
-    # Set a min area threshold:
-    minArea = 25
+    # Set a min area threshold (there are 2 objects with an area equal to 0)
+    minArea = 0
 
     if currentArea > minArea:
 
+        print("currentArea = " + str(currentArea))
         # Approximate the contour to a polygon:
         contoursPoly = cv2.approxPolyDP(c, 3, True)
         # Get the polygon's bounding rectangle:
@@ -48,12 +53,9 @@ for _, c in enumerate(contours):
         color = (0, 0, 255)
         cv2.rectangle(img, (int(rectX), int(rectY)),
                    (int(rectX + rectWidth), int(rectY + rectHeight)), color, 2 )
-        
 
-        
-
-cv2.imshow("Rectangles", img)
+cv2.imshow("Logo detection", img)
 cv2.waitKey(0)
 
-print(len(rectanglesList))
+print("The logo is composed by " + str(len(rectanglesList)) + "objects")
 
